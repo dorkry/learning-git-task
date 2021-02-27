41ffa20aed8576f746192f7d49e72f37caf181ab
Dorota Krygowska <dorota.krygowska@gmail.com>
2021-02-27 21:45:55 +0100

    spacje

--- lista_zakup2.py
+++ lista_zakup2.py
@@ -1,18 +1,20 @@
 #lista zakupów
-shopping={
+shopping = {
     "Piekarnia":["chleb", "pączek", "bułki"],
     "Warzywniak":["marchew", "seler", "rukola"]
-    }
-number=0
+}
+number = 0
 #new comment to commit to Git
 for shop in shopping.keys():
-    products=""
+    products = ""
     for i in shopping[shop]:
-        products=products  + i.capitalize()+","
-    print("Idę do ",shop.capitalize(), "i kupuję ",products)
-    number=number+len(shopping[shop])
+        products = products + i.capitalize() + ","
+    print("Idę do ", shop.capitalize(), "i kupuję ", products)
+    number = number + len(shopping[shop])
 #the end-second commit
 #greetings
 print("Greetings for Trinidad and Tobago!")
+#poprawione spacje
+
 
     
