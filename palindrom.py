2804f9674b4b7b1b5716dbaaeae47ae3bcea634d
Dorota Krygowska <dorota.krygowska@gmail.com>
2021-02-28 21:30:11 +0100

    help

--- palindrom.py
+++ palindrom.py
@@ -1,19 +1,26 @@
 #palindromy
 
-def palindrom(text):
-    i=int(len(text)/2) #połowa długości tekstu
+def palindrome(text):
+    """
+        Checks if given argument is palindrome. Returns True or False
+        Arguments:
+        text - string to be checked
+    """
+    i=int(len(text)/2) #half of text length
     for index in range(i):
-        if text[index]==text[-(index+1)]: #sprawdzanie kolejnych znaków
+        if text[index]==text[-(index+1)]: #check characters
             continue
         else:
             return False
     return True
 
 
-#sprawdzenie działania funkcji
+#check
 to_check=['ada', 'kajak', 'pies', 'zakaz', 'abccba', 'drzewo', 'radar', 'abba', 'oko', 'dziecko']
 for word in (to_check):
-    if(palindrom(word)):
+    if(palindrome(word)):
         print(word, " jest palindromem")
     else:
         print(word, " nie jest palindromem")
+
+help(palindrome)
