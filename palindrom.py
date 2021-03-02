a983bd1202010bf061a40bedbbe377ee4602e112
Dorota Krygowska <dorota.krygowska@gmail.com>
2021-03-02 22:37:36 +0100

    spacje

--- palindrom.py
+++ palindrom.py
@@ -6,9 +6,9 @@ def palindrome(text):
         Arguments:
         text - string to be checked
     """
-    i=int(len(text)/2) #half of text length
+    i = int(len(text) / 2) #half of text length
     for index in range(i):
-        if text[index]==text[-(index+1)]: #check characters
+        if text[index] == text[-(index + 1)]: #check characters
             continue
         else:
             return False
@@ -16,7 +16,7 @@ def palindrome(text):
 
 
 #check
-to_check=['ada', 'kajak', 'pies', 'zakaz', 'abccba', 'drzewo', 'radar', 'abba', 'oko', 'dziecko']
+to_check = ['ada', 'kajak', 'pies', 'zakaz', 'abccba', 'drzewo', 'radar', 'abba', 'oko', 'dziecko']
 for word in (to_check):
     if(palindrome(word)):
         print(word, " jest palindromem")
