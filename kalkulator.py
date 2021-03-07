df371b0e4ac7661146793398a67621c78ad03cf8
Dorota Krygowska <dorota.krygowska@gmail.com>
2021-03-07 22:41:35 +0100

    funkcje

--- kalkulator.py
+++ kalkulator.py
@@ -2,20 +2,36 @@
 import logging
 logging.basicConfig(level=logging.INFO)
 
-print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
-operation = input("Wybierz działanie: ")
-a = float(input("Podaj liczbę: "))
-b = float(input("Podaj drugą liczbę: "))
+def getdata():
+    print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+    operation = input("Wybierz działanie: ")
+    a = float(input("Podaj liczbę: "))
+    b = float(input("Podaj drugą liczbę: "))
+    return (operation, a, b)
+
+def add(x, y):
+    return x + y
+
+def substract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+(operation, a, b) = getdata()
 
 if operation == "1":
     logging.info(f"Dodaję {a} i {b}")
-    print(a + b)
+    print(add(a, b))
 elif operation == "2":
     logging.info(f"Odejmuję {b} od {a}")
-    print(b - a)
+    print(substract(a, b))
 elif operation == "3":
     logging.info(f"Mnożę {a} i {b}")
-    print(a * b)
+    print(multiply(a, b))
 elif operation == "4":
     logging.info(f"Dzielę {a} przez {b}")
-    print(a / b)
+    print(divide(a, b))
