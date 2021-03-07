3cb7608b2474925da32a5ba512ff49e4965514b7
Dorota Krygowska <dorota.krygowska@gmail.com>
2021-03-07 23:05:14 +0100

    pętla

--- kalkulator.py
+++ kalkulator.py
@@ -3,10 +3,14 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 def getdata():
-    print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+    print("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie, 0 Koniec:")
     operation = input("Wybierz działanie: ")
-    a = float(input("Podaj liczbę: "))
-    b = float(input("Podaj drugą liczbę: "))
+    if operation != "0":
+        a = float(input("Podaj liczbę: "))
+        b = float(input("Podaj drugą liczbę: "))
+    else:
+        a = 0
+        b = 0
     return (operation, a, b)
 
 def add(x, y):
@@ -21,17 +25,22 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-(operation, a, b) = getdata()
+if __name__ == "__main__":
+    while True:
+        (operation, a, b) = getdata()
 
-if operation == "1":
-    logging.info(f"Dodaję {a} i {b}")
-    print(add(a, b))
-elif operation == "2":
-    logging.info(f"Odejmuję {b} od {a}")
-    print(substract(a, b))
-elif operation == "3":
-    logging.info(f"Mnożę {a} i {b}")
-    print(multiply(a, b))
-elif operation == "4":
-    logging.info(f"Dzielę {a} przez {b}")
-    print(divide(a, b))
+        if operation == "1":
+            logging.info(f"Dodaję {a} i {b}")
+            print(add(a, b))
+        elif operation == "2":
+            logging.info(f"Odejmuję {b} od {a}")
+            print(substract(a, b))
+        elif operation == "3":
+            logging.info(f"Mnożę {a} i {b}")
+            print(multiply(a, b))
+        elif operation == "4":
+            logging.info(f"Dzielę {a} przez {b}")
+            print(divide(a, b))
+        elif operation == "0":
+            logging.info("Koniec")
+            break
