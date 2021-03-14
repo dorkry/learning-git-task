3d07fac74fdf4154d7b0b3f263aa523d654d46de
Dorota Krygowska <dorota.krygowska@gmail.com>
2021-03-14 18:55:03 +0100

    version2

--- magazyn.py
+++ magazyn.py
@@ -13,8 +13,12 @@ menu = {
 if __name__ == "__main__":
     while True:
         choice = input("What do you want to do?")
-        menu_func = menu[choice]
-        result = menu_func()
-        if result == 0:
-            print("Thank you! Bye!")
-            break
+        try:
+            menu_func = menu[choice]
+        except KeyError:
+            print("Unknown action")
+        else:    
+            result = menu_func()
+            if result == 0:
+                print("Thank you! Bye!")
+                break
